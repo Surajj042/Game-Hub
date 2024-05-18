9f79bed98d689ee1263f4a6a3cecf776680cba90
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-05-18 23:25:32 +0545

    Displaying Platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,34 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image:string;
-  }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms:{platform: Platform}[];
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-    
-    useEffect(() => {
-      const controller =new AbortController();
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
     apiClient
-      .get<FetchGamesResponse>("/games",{signal:controller.signal})
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) =>{
-        if(err instanceof CanceledError) return;
-        setError(err.message)});
-          
-      return()=>controller.abort();
-  },[]);
-  return {games,error};
-}
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+  return { games, error };
+};
 
 export default useGames;
