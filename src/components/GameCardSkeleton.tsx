4ce818628a44a76a2b60fec3285544e0e21041c8
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-05-19 00:07:21 +0545

    Changing width of Card

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
-    <Card width='300px' borderRadius={10} overflow='hidden'>
+    <Card width='275px' borderRadius={10} overflow='hidden'>
       <Skeleton height="200px" />
       <CardBody>
         <SkeletonText/>
