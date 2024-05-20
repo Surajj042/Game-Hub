c1d038ab588228849bc493df2d57e3a12762769a
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-05-20 13:26:28 +0545

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,13 @@
+import useGenres from './useGenres';
+
+const GenreList = () => {
+    const {genres}=useGenres();
+  return (
+    <ul>
+        {genres.map(genre=><li key={genre.id}>{genre.name}</li>)}
+      
+    </ul>
+  )
+}
+
+export default GenreList
