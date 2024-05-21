81691c19c12b998cad5c32e87334ed395fcecbd4
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-05-21 23:25:44 +0545

    Refactor:Extract a query objects.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,5 +1,4 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
-import React from 'react'
 import { BsChevronDown } from 'react-icons/bs'
 import usePlatforms from '../hooks/usePlatforms';
 import { Platform } from '../hooks/useGames';
