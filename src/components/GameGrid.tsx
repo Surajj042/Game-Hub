2acc5a5a9afcc14eaea7c40c8c4a6bd4044ff47b
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-05-21 16:45:15 +0545

    Filter Games by Platforms

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -6,11 +6,12 @@ import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
 
 interface Props{
-  selectedGenre:Genre|null
+  selectedGenre:Genre|null;
+  selectedPlatform:Platform|null;
 }
 
-const GameGrid = ({selectedGenre}:Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({selectedGenre,selectedPlatform}:Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre,selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
