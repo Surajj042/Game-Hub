823e12d200643ade2a3d574aafa97526b454fcf8
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-06-04 16:23:56 +0545

    Refactoring-Remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,4 +1,4 @@
-import { Platform } from "../hooks/useGames";
+import { Platform } from "../hooks/usePlatforms";
 import { HStack, Icon } from "@chakra-ui/react";
 import {
   FaWindows,
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,9 +1,8 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
-import usePlatforms from '../hooks/usePlatforms';
-import { Platform } from '../hooks/useGames';
+import usePlatforms,{ Platform } from '../hooks/usePlatforms';
 
-interface Props{
+export interface Props{
     onSelectPlatform:(platform:Platform)=>void;
     selectedPlatform:Platform|null;
 }
