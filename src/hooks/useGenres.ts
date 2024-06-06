cb7a80d46f6ef054c9c54ab5048ed1c63cf5763a
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-06-06 15:42:09 +0545

    Refactor - Create Lookup Hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APIClient, { FetchResponse } from "../services/api-client";
 
-const apiClinet = new APIClient<Genre>('/genres')
+const apiClinet = new APIClient<Genre>("/genres");
 
 export interface Genre {
   id: number;
@@ -14,8 +14,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClinet.getAll,
-    staleTime:24*60*60*1000,// 24hr
-    initialData: {count:genres.length,results:genres}
+    staleTime: 24 * 60 * 60 * 1000, // 24hr
+    initialData: genres,
   });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
 
-const apiClient = new APIClient<Platform>('platforms/lists/parents')
+const apiClient = new APIClient<Platform>("platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -12,10 +12,10 @@ export interface Platform {
 
 const usePlatforms = () =>
   useQuery({
-    queryKey: ['platforms'],
-    queryFn:apiClient.getAll,
-    staleTime:24*60*60*1000,//24hr
-    initialData:{count:platforms.length,results:platforms},
+    queryKey: ["platforms"],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000, //24hr
+    initialData: platforms,
   });
 
 export default usePlatforms;
