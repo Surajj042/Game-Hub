286047c6ae671c3e8e863ace12ac30d883aaa9c0
Surajj042 <suraj.gurung.sg98@gmail.com>
2024-06-08 23:21:19 +0545

    Refactor entity interfaces

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,7 +8,7 @@ import {
   Spinner,
 } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
-import { Genre } from "../entities/Genre";
+import Genre  from "../entities/Genre";
 import getCroppedImageUrl from "../services/image-url";
 import useGameQueryStore from "../services/store";
 
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,4 +1,4 @@
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import { HStack, Icon } from "@chakra-ui/react";
 import {
   FaWindows,
